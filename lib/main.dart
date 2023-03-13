9c015c17ca3348141777443dea164730eb3f8fc3
José <j.eduardo.alves@hotmail.com>
2023-03-13 19:14:05 -0300

    commit inicial

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:cidade_fav/favority_city.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    title: "Exemplo com monitoramento de estado",
+    home: FavorityCity(),
+  ));
+}
